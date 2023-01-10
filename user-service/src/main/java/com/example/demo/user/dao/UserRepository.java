b4aca63d2cdf6aeff43afb3f92e8f592f84e79ab
adriantarina <tarina.adrian@gmail.com>
2023-01-10 16:57:00 +0200

    Added spring-data-rest for generating endpoints

--- user-service/src/main/java/com/example/demo/user/dao/UserRepository.java
+++ user-service/src/main/java/com/example/demo/user/dao/UserRepository.java
@@ -4,7 +4,6 @@ import com.example.demo.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
